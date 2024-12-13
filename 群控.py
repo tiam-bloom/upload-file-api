0348354e5e7c6747d69adbe4e7422ba3eff82736
yujing <3036293856@qq.com>
2024-12-14 00:05:01 +0800

    winrm 远控

---
+++ 群控.py
@@ -0,0 +1,22 @@
+
+# python3.8 pip install pywinrm
+import winrm
+# 创建远程会话
+remote_computer = "http://192.168.31.134:5985/wsman"  # 目标设备的 WinRM 地址
+username = "Administrator"
+password = "123456"
+
+# winrs -r:192.168.31.134:5985 -u:Administrator -p:123456 ipconfig
+
+session = winrm.Session(remote_computer, auth=(username, password),  transport="ntlm")
+# from requests_kerberos import HTTPKerberosAuth
+# session = winrm.Session(remote_computer, auth= HTTPKerberosAuth())
+
+# 执行命令
+command = "ipconfig /all"
+response = session.run_cmd(command)
+
+print(response.status_code)
+# 打印结果
+print("STDOUT:", response.std_out.decode("ISO-8859-1"))
+# print("STDERR:", response.std_err.decode())
